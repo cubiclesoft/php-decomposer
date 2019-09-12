6ec0d8ab80174b4b9f36f646090aeac93956e599
cubiclesoft <webmaster@cubiclesoft.com>
2019-09-12 07:17:27 -0700

    Moved to PHP Minifier library.

--- decomposer.php
+++ decomposer.php
@@ -213,139 +213,25 @@
 			}
 			closedir($dir);
 
-			function DumpTokens($tokens)
-			{
-				foreach ($tokens as $token)
-				{
-					if (is_array($token))  echo "Line " . $token[2] . ": " . token_name($token[0]) . " | " . $token[1] . "\n";
-					else  echo "Str:  " . $token . "\n";
-				}
-			}
+			require_once $rootpath . "/support/php_minifier.php";
 
 			$warnings = array();
 			function ProcessNamespacedFile($file, $requirenamespace)
 			{
 				global $warnings;
 
-				$tokens = token_get_all(file_get_contents($file));
-
-				// Remove all comments and change spaces to tabs to considerably reduce PHP file size.
-				foreach ($tokens as $num => $token)
-				{
-					if (is_array($token) && ($token[0] === T_COMMENT || $token[0] === T_DOC_COMMENT))  unset($tokens[$num]);
-					else if (is_array($token) && $token[0] === T_WHITESPACE)  $tokens[$num][1] = str_replace("\r", "\n", str_replace("\r\n", "\n", str_replace("  ", "\t", str_replace("   ", "\t", str_replace("    ", "\t", $token[1])))));
-				}
-				$tokens = array_values($tokens);
-
-				// Remove starting and trailing PHP tags and whitespace.
-				while (count($tokens) && is_array($tokens[0]) && ($tokens[0][0] === T_OPEN_TAG || $tokens[0][0] === T_WHITESPACE))  array_shift($tokens);
-				while (count($tokens) && is_array($tokens[count($tokens) - 1]) && ($tokens[count($tokens) - 1][0] === T_CLOSE_TAG || $tokens[count($tokens) - 1][0] === T_WHITESPACE))  array_pop($tokens);
-
-				// Determine namespace used (if any).
-				$namespaced = (count($tokens) && is_array($tokens[0]) && $tokens[0][0] === T_NAMESPACE);
-				if ($namespaced)
-				{
-					// Change namespace ';' to '{' and append a '}'.
-					foreach ($tokens as $num => $token)
-					{
-						if (!is_array($token))
-						{
-							if ($token === "{")  break;  // }
-							else if ($token === ";")
-							{
-								$tokens[$num] = " {";
-								$tokens[] = "\n}";
-
-								break;
-							}
-						}
-					}
-				}
-				else
-				{
-					if ($requirenamespace)  return false;
-				}
-
-				// Process warnings.
-				$warnstrs = array(
-					"opendir" => true,
-					"readdir" => true,
-					"closedir" => true,
-					"scandir" => true,
-					"glob" => true,
-					"directoryiterator" => true,
-					"recursivedirectoryiterator" => true,
+				$options = array(
+					"require_namespace" => $requirenamespace,
+					"check_dir_functions" => true,
+					"wrap_includes" => true
 				);
 
-				foreach ($tokens as $num => $token)
-				{
-					if (is_array($token) && $token[0] === T_STRING && isset($warnstrs[strtolower($token[1])]))
-					{
-						$warnings[] = "Found '" . $token[1] . "' in '" . $file . "' on line " . $token[2] . ".";
-					}
+				$result = PHPMinifier::Minify($file, file_get_contents($file), $options);
+				if (!$result["success"])  return false;
 
-					if (is_array($token) && ($token[0] === T_REQUIRE || $token[0] === T_REQUIRE_ONCE || $token[0] === T_INCLUDE || $token[0] === T_INCLUDE_ONCE))
-					{
-						$warnings[] = "Found '" . $token[1] . "' in '" . $file . "' on line " . $token[2] . ".";
+				foreach ($result["warnings"] as $warning)  $warnings[] = $warning;
 
-						// Determine what string follows and inject a file_exists() wrapper.
-						$y = count($tokens);
-						$filename = "";
-						for ($x = $num + 1; $x < $y; $x++)
-						{
-							if (!is_array($tokens[$x]) && $tokens[$x] === ";")
-							{
-								$tokens[$x] .= " }";
-
-								break;
-							}
-							else if (is_array($tokens[$x]) && $tokens[$x][0] === T_CLOSE_TAG)
-							{
-								$tokens[$x][1] = "} " . $tokens[$x][1];
-
-								break;
-							}
-
-							$filename .= (is_array($tokens[$x]) ? $tokens[$x][1] : $tokens[$x]);
-						}
-
-						$tokens[$num][1] = "{ if (file_exists(" . trim($filename) . "))  " . $tokens[$num][1];
-					}
-				}
-
-				// Condense the data.
-				$result = "<" . "?php\n" . ($namespaced ? "" : "namespace {\n");
-				foreach ($tokens as $token)
-				{
-					$result .= (is_array($token) ? $token[1] : $token);
-				}
-				if (!$namespaced)  $result .= "}";
-
-				// Clean up and remove spurious empty lines and trailing whitespace.
-				$tokens = token_get_all($result);
-
-				// Remove starting PHP tags and whitespace (again).
-				while (count($tokens) && is_array($tokens[0]) && ($tokens[0][0] === T_OPEN_TAG || $tokens[0][0] === T_WHITESPACE))  array_shift($tokens);
-
-				foreach ($tokens as $num => $token)
-				{
-					if (is_array($token) && $token[0] === T_WHITESPACE)
-					{
-						$lines = explode("\n", $token[1]);
-
-						if (count($lines) > 2)  $tokens[$num][1] = "\n\n" . $lines[count($lines) - 1];
-						else if (count($lines) == 2)  $tokens[$num][1] = "\n" . $lines[count($lines) - 1];
-					}
-				}
-
-				// Prepare the result.
-				$result = "";
-				foreach ($tokens as $token)
-				{
-					$result .= (is_array($token) ? $token[1] : $token);
-				}
-
-				return $result;
+				return $result["data"];
 			}
 
 			function FindPHPFiles($path)
